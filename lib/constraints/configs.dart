b43b388121d873c46e4f7996529d7a5c077d4a46
Mazharul Sabbir <42198761+mazharulsabbir@users.noreply.github.com>
2023-01-31 08:52:32 +0600

    Create configs.dart

---
+++ lib/constraints/configs.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  final String _oneSignalAppId = "YOUR_ONESIGNAL_APP_ID";
+  final String _oneSignalRestAPIKey = "YOUR_ONESIGNAL_REST_API_KEY";
+
+  String get oneSignalAppId => _oneSignalAppId;
+  String get oneSignalRestAPIKey => _oneSignalRestAPIKey;
+}
